54285fe581d02cdfd11bdc2df585fffb597cf2e7
Atharva Khole <atharvakhole26@gmail.com>
2024-05-07 21:13:27 +0800

    Install: Material UI

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,23 +1,41 @@
 import { Component } from "react";
-import { Navigate } from "react-router-dom";
 import axios from "../api/axios";
 import { withRouter } from "../withRouter";
+
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { InputLabel } from "@mui/material";
+import { FormControl, MenuItem, Select } from "@mui/base";
 
 class LoginPage extends Component {
 
     state;
+    defaultTheme;
 
     constructor(props) {
         super(props)
         this.state = {
             email: '',
             password: '',
+            accountType: '',
             errorMessage: '',
             setAuth: props.auth.setAuth,
             navigate: props.navigate,
             location: props.location,
             from: props.location.state?.from?.pathname || "/"
         }
+        this.defaultTheme = createTheme();
         console.log("login.props : ", props)
         console.log("login.state.from : ", this.state.from)
     }
@@ -27,6 +45,7 @@ class LoginPage extends Component {
     }
 
     handleChange = (e) => {
+        console.log("HandleChange: ", e);
         this.setState({
             [e.target.name]: e.target.value
         });
@@ -36,7 +55,7 @@ class LoginPage extends Component {
         e.preventDefault();
         const { email, password } = this.state;
         try {
-            const response = await axios.post('/systemadmin/login',
+            const response = await axios.post(`/${this.state.accountType}/login`,
                 JSON.stringify({ email, password }), {
                 headers: { 'Content-Type': 'application/json' },
                 withCredentials: true
@@ -63,37 +82,112 @@ class LoginPage extends Component {
         }
     };
 
+
     render() {
-        const { email, password, loggedIn } = this.state;
-        if (loggedIn) {
-            // Redirect to Home
-        }
+        const { email, password, accountType } = this.state;
         return (
-            <div>
-                <h2>Login</h2>
-                <form onSubmit={this.handleSubmit}>
-                    <input
-                        type="text"
-                        name="email"
-                        value={email}
-                        onChange={this.handleChange}
-                        placeholder="Email"
-                    />
-                    <br />
-                    <input
-                        type="password"
-                        name="password"
-                        value={password}
-                        onChange={this.handleChange}
-                        placeholder="Password"
-                    />
-                    <br />
-                    <button type="submit">Login</button>
-                </form>
-                <div>{this.state.errorMessage}</div>
-            </div>
+            <ThemeProvider theme={this.defaultTheme}>
+                <Container component="main" maxWidth="xs">
+                    <CssBaseline />
+                    <Box
+                        sx={{
+                            marginTop: 8,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                            <LockOutlinedIcon />
+                        </Avatar>
+                        <Typography component="h1" variant="h5">
+                            Sign in
+                        </Typography>
+                        <Box component="form" onSubmit={this.handleSubmit} noValidate sx={{ mt: 1 }}>
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email Address"
+                                name="email"
+                                autoComplete="email"
+                                autoFocus
+                            />
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                name="password"
+                                label="Password"
+                                type="password"
+                                id="password"
+                                autoComplete="current-password"
+                            />
+                            <InputLabel >Account Type</InputLabel>
+                            <select name="accountType" value={accountType} onChange={this.handleChange}>
+                                <option value="">Select...</option>
+                                <option value="rea">Real Estate Agent</option>
+                                <option value="buyer">Buyer</option>
+                                <option value="seller">Seller</option>
+                                <option value="systemadmin">System Administrator</option>
+                            </select>
+                            <br />
+                            <FormControl fullWidth>
+                                <InputLabel id="demo-simple-select-label">Age</InputLabel>
+                                <Select
+                                    labelId="demo-simple-select-label"
+                                    id="demo-simple-select"
+                                    value={accountType}
+                                    label="Age"
+                                    onChange={this.handleChange}
+                                >
+                                    <MenuItem value={10}>Ten</MenuItem>
+                                    <MenuItem value={20}>Twenty</MenuItem>
+                                    <MenuItem value={30}>Thirty</MenuItem>
+                                </Select>
+                            </FormControl>
+                            <FormControlLabel
+                                control={<Checkbox value="remember" color="primary" />}
+                                label="Remember me"
+                            />
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                sx={{ mt: 3, mb: 2 }}
+                            >
+                                Sign In
+                            </Button>
+                            <Grid container>
+                                <Grid item xs>
+                                    <Link href="#" variant="body2">
+                                        Forgot password?
+                                    </Link>
+                                </Grid>
+                                <Grid item>
+                                    <Link href="#" variant="body2">
+                                        {"Don't have an account? Sign Up"}
+                                    </Link>
+                                </Grid>
+                            </Grid>
+                        </Box>
+                    </Box>
+                    <Copyright sx={{ mt: 8, mb: 4 }} />
+                </Container>
+            </ThemeProvider>
         );
     }
+}
+
+const Copyright = (props) => {
+    return (
+        <Typography variant="body2" color="text.secondary" align="center" {...props}>
+            {'Copyright © '}
+            Grad2025 Group - Real Estate System
+            {'.'}
+        </Typography>
+    );
 }
 
 export default withRouter(LoginPage);
--- frontend/src/pages/RealEstateAgentHomePage.js
+++ frontend/src/pages/RealEstateAgentHomePage.js
@@ -17,45 +17,8 @@ class RealEstateAgentHomePage extends Component {
     // @UserStory
     // As a Real Estate Agent, I want to search other property listings
     // to keep track of the market.
-    handleSearch = (_e) => {
-        e.preventDefault();
-        const { title, listingAgentID, sellerID, description, propertyType, livingArea, numberOfBedrooms, numberOfBathrooms, listPrice } = this.state;
-        try {
-            const response = await axios.post('/rea/search',
-                JSON.stringify({
-                    title,
-                    listingAgentID,
-                    sellerID,
-                    description,
-                    propertyType,
-                    livingArea,
-                    numberOfBedrooms,
-                    numberOfBathrooms,
-                    listPrice
-                }), {
-                headers: { 'Content-Type': 'application/json' },
-                withCredentials: true
-            }
-            );
-            console.log("API Response: ", response?.data);
-            this.state.setAuth({
-                firstName: response?.data?.firstName,
-                accountType: response?.data?.accountType,
-                token: response?.data?.token,
-            })
-            this.state.navigate(this.state.from, { replace: true });
-        } catch (err) {
-            console.log("ERROR: ", err?.response);
-            if (err?.response) {
-                this.setState({
-                    errorMessage: err.response.data.message
-                });
-            } else {
-                this.setState({
-                    errorMessage: "No response from server"
-                });
-            }
-        }
+    handleSearch = async(_e) => {
+        _e.preventDefault();
     }
 
     render() {
